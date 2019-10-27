86ec6beed37438ad265e500ca29a7b6d4389eb3e
Ben Lavalley <ben.lavalley@kaseya.com>
2019-10-27 14:53:06 -0400

    updates to use imports/exports, linting, renamed Inject to InjectInitial

--- gadicohen:inject-initial/package.js
+++ gadicohen:inject-initial/package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  summary: "Deprecated.  Use meteorhacks:inject-initial instead.",
-  version: "1.0.2",
-  git: "https://github.com/meteorhacks/meteor-inject-initial.git",
-  name: "gadicohen:inject-initial"
+	summary: 'Deprecated.  Use meteorhacks:inject-initial instead.',
+	version: '1.0.2',
+	git: 'https://github.com/meteorhacks/meteor-inject-initial.git',
+	name: 'gadicohen:inject-initial',
 });
 
 Package.on_use(function (api) {
-  api.use('meteorhacks:inject-initial@1.0.2');
-  api.imply('meteorhacks:inject-initial');
+	api.use('meteorhacks:inject-initial@1.0.5');
+	api.imply('meteorhacks:inject-initial');
 
-  // XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
-  //
-  // (in particular, packages that have a weak dependency on this
-  // package, since then exported symbols live on the
-  // `Package[gadicohen:inject-initial]` object)
-  api.export('Inject', 'server');
-  api.export(['Injected', 'Inject'], 'client');
+	// XXX COMPAT WITH PACKAGES BUILT FOR 0.9.0.
+	//
+	// (in particular, packages that have a weak dependency on this
+	// package, since then exported symbols live on the
+	// `Package[gadicohen:inject-initial]` object)
+	api.export('Inject', 'server');
+	api.export(['Injected', 'Inject'], 'client');
 });
--- package.js
+++ package.js
@@ -1,41 +1,42 @@
 Package.describe({
-  summary: "Allow injection of arbitrary data to initial Meteor HTML page",
-  version: "1.0.4",
-  git: "https://github.com/meteorhacks/meteor-inject-initial.git",
-  name: "meteorhacks:inject-initial"
+	summary: 'Allow injection of arbitrary data to initial Meteor HTML page',
+	version: '1.0.5',
+	git: 'https://github.com/meteorhacks/meteor-inject-initial.git',
+	name: 'meteorhacks:inject-initial',
 });
 
 Npm.depends({
-  "connect": "2.12.0"
+	connect: '3.7.0',
 });
 
-Package.on_use(function (api) {
-  configurePackage(api);
-	api.export('Inject', 'server');
+Package.onUse(function (api) {
+	configurePackage(api);
+	api.export('InjectInitial', 'server');
 	api.export(['Injected', 'Inject'], 'client');
 });
 
-Package.on_test(function(api) {
-  configurePackage(api);
-  api.use('tinytest', ['client', 'server']);
+Package.on_test(function (api) {
+	configurePackage(api);
+	api.use('tinytest', ['client', 'server']);
 
-  api.add_files('test/inject-helpers.js', 'server');
-  api.add_files('test/inject-public-api.js', 'server');
-  api.add_files('test/inject-internal-api.js', 'server');
-  api.add_files('test/inject-core.js', 'server');
+	api.addFiles('test/inject-helpers.js', 'server');
+	api.addFiles('test/inject-public-api.js', 'server');
+	api.addFiles('test/inject-internal-api.js', 'server');
+	api.addFiles('test/inject-core.js', 'server');
 
-  api.add_files('test/injected-public-api.js', ['server', 'client']);
+	api.addFiles('test/injected-public-api.js', ['server', 'client']);
 });
 
 function configurePackage(api) {
-  if(api.versionsFrom) {
-    api.versionsFrom('METEOR@1.6.1');
-  }
-  
-  api.use(['routepolicy', 'webapp'], 'server');
-  api.use(['ejson', 'underscore'], ['client','server']);
+	if (api.versionsFrom) {
+		api.versionsFrom('METEOR@1.8.1');
+	}
 
-  api.add_files('lib/inject-server.js', 'server');
-  api.add_files('lib/inject-core.js', 'server');
-  api.add_files('lib/inject-client.js', 'client');
+	api.use(['routepolicy', 'webapp'], 'server');
+	api.use(['ejson', 'underscore', 'ecmascript'], ['client', 'server']);
+	// api.mainModule('lib/namespace.js', ['server', 'client']);
+	api.mainModule('lib/namespace.js', 'server');
+	api.addFiles('lib/inject-server.js', 'server');
+	api.addFiles('lib/inject-core.js', 'server');
+	api.addFiles('lib/inject-client.js', 'client');
 }
